53dfd244b500553a74c5f5d8f5fec6f03b62e3ff
Gautam Varma Mantena <gautam.mantena@gmail.com>
2016-05-04 17:00:00 +0800

    Update run.sh

--- egs/run.sh
+++ egs/run.sh
@@ -6,8 +6,8 @@ feat_transform=kaldi-data-models/tri3a_dnn/final.feature_transform
 dbn_model=kaldi-data-models/tri3a_dnn/final.nnet
 dir=data-files
 
-#extract-activity-vectors --use-gpu="yes" --S=40 --buffer-index=0 \
-extract-activity-vectors --S=40 --buffer-index=0 \
+#extract-activity-vectors --S=40 --buffer-index=0 \
+extract-activity-vectors --use-gpu="yes" --S=40 --buffer-index=0 \
   --feature-transform=$feat_transform $dbn_model \
   scp:$dir/dev_feats.scp ark:$dir/dev_0330.monophones.alignment ark,t:activation-vectors.ark || exit 1
 
